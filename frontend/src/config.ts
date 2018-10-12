0d8f6ef772580ec55d20edcf8fa0e72a56a0862f
Michael Rojas <mikerojaswa@gmail.com>
2018-10-11 20:32:13 -0700

    add api

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ let baseApiUrl;
 if (process.env.NODE_ENV === "development") {
   baseApiUrl = "http://127.0.0.1:8000";
 } else {
-  baseApiUrl = "http://rojasworld.com";
+  baseApiUrl = "http://api.rojasworld.com";
 }
 
 export {
